fb4185fddd29c21f5776610d6529c9fd60d3f251
Нигer <104298650+Domashnirabota@users.noreply.github.com>
2022-05-08 13:31:28 +0300

    Add files via upload

--- l.py
+++ l.py
@@ -20,15 +20,61 @@ class Player(GameSprite):
         if keys[K_RIGHT] and self.rect.x < 595:
             self.rect.x += self.speed
             window = display.set_mode((700, 500))
+from pygame import *
 
-window = display.set_mode((500, 247))
-display.set_caption('Лабиринт')
-background = transform.scale(image.load('doramap.png'),(500, 247))
+from random import randint
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.player_image = player_image
+        self.image = transform.scale(image.load(player_image),(width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 50:
+            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 400:
+            self.rect.y += self.speed
+
+
+class Player2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 600:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 650:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed
+
+
+
+        
+
+window = display.set_mode((700, 500))
+display.set_caption('пинпонг')
+background = transform.scale(image.load('doramap.png'),(700, 500))
 clock = time.Clock()
 FPS = 160
 game = True
 finish = False
-
+player = Player('Malchik.jpg', 1, 250, 3, 30, 200)
+player2 = Player2('Malchik.jpg', 600, 250, 3, 30, 200)
 
 
 while game:   
@@ -39,6 +85,8 @@ while game:
         window.blit(background,(0, 0))
         player.reset()
         player.update()
+        player2.reset()
+        player2.update()
 
     clock.tick(FPS)
     display.update()
